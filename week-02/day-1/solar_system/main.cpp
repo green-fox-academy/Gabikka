81c2a39968da29cd300f4b0824a0484af81aee0e
Gábor Nagy <levelgabonak@gmail.com>
2019-01-22 08:40:04 +0100

    find and insert

---
+++ week-02/day-1/solar_system/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> putSaturn(const std::vector<std::string>& planets)
+{
+    std::vector<std::string> correctListOfPlanets = planets;
+    correctListOfPlanets.insert(correctListOfPlanets.begin()+5, "Saturn");
+    return correctListOfPlanets;
+}
+
+int main(int argc, char* args[])
+{
+    std::vector<std::string> planets = {"Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"};
+
+    // Saturn is missing from the planetList
+    // Insert it into the correct position
+    // Create a method called putSaturn() which has list parameter and returns the correct list.
+
+    // Expected output: Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune
+
+    std::vector<std::string> correctPlanets = putSaturn(planets);
+
+    for(int i = 0; i < correctPlanets.size(); ++i) {
+        std::cout << correctPlanets[i] << " ";
+    }
+
+    return 0;
+}
