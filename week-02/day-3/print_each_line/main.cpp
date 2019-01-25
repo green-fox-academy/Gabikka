ede18bb7c0840fba01f76b6514c5214b55756cc8
Gábor Nagy <levelgabonak@gmail.com>
2019-01-25 08:44:28 +0100

    done

---
+++ week-02/day-3/print_each_line/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main () {
+    // Write a program that opens a file called "my-file.txt", then prints
+    // each line from the file.
+    // You will have to create the file, you may use C-programming, although it is not mandatory
+
+
+    std::ifstream myFile;
+    myFile.open("my-text.txt");
+
+    std::string lines;
+    while (std::getline(myFile, lines)) {
+        std::cout << lines << "\n";
+       };
+
+      return 0;
+}
