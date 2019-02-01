869f61747f2d9c63654c2823e84c20835ff1652e
Gábor Nagy <levelgabonak@gmail.com>
2019-02-01 12:01:21 +0100

    done

---
+++ week-03/day-3/fleet_of_things/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "fleet.h"
+
+int main(int argc, char* args[])
+{
+    Fleet fleet;
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+
+
+   Thing getMilk("Get milk");
+   Thing removeObstacle("Remove the obstacles");
+   Thing standUp("Stand up");
+   Thing eatLunch("Eat lunch");
+
+   standUp.complete();
+   eatLunch.complete();
+
+   fleet.add(getMilk);
+   fleet.add(removeObstacle);
+   fleet.add(standUp);
+   fleet.add(eatLunch);
+
+   std::cout << fleet.toString() << std::endl;
+    return 0;
+}
